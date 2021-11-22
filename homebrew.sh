a60f7fa1886376cef20630d515d11681a832ddc2
Adrian Zygadlo <adrian@zyian.dev>
2021-11-21 22:17:58 -0500

    :broom: cleaned up homebrew install

--- homebrew.sh
+++ homebrew.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Install command-line tools using Homebrew.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+fi;
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -21,31 +23,26 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
+# Update from mac default zsh
+brew install zsh
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Switch to using brew-installed zsh as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/zsh";
+fi;
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install tmux
-# brew install php
-# brew install gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -57,9 +54,6 @@ brew install --cask iterm2
 brew install --cask fantastical
 brew install --cask rectangle
 brew install --cask alfred
-brew install --cask firefox
-brew install --cask vlc
-brew install --cask keybase
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 # brew install aircrack-ng
@@ -89,22 +83,9 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
 brew install gs
-brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rlwrap
-# brew install ssh-copy-id
-# brew install tree
-# brew install vbindiff
-# brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
